c325623c532858f5548c9e5586ccd6b1a26bb450
Dee Davis <threedaycleanse@gmail.com>
2017-03-21 23:06:10 -0400

    Data Model

---
+++ homework/data_modeling_hw/server.js
@@ -0,0 +1,39 @@
+var exampleBook = {
+  name: "Motivation",
+  date: "March 14, 2016",
+  pages: 351,
+  publisher: "Author",
+  isbnNumber = 9763141421546,
+  category: "Self-help",
+  writer: [{
+    id: 39,
+    name: "Anonymous",
+    address: "Florida",
+    phoneNumber = 2225556252
+  }]
+}
+
+var exampleWriter = {
+  id: 39,
+  name: "Anonymous",
+  address: "Florida",
+  phoneNumber = 2225556252
+}
+
+var coffeeShops = {
+  name: "",
+  location: "Midtown",
+  amenities: "Wi-fi",
+  coffee: [{
+    size: "Medium",
+    type: "Blend",
+    flavor "Vanilla",
+}]
+}
+
+var exampleUsers = {
+  id: 32,
+  name: "";
+  address: "Any Street",
+  email: "",
+  payOption: true;
