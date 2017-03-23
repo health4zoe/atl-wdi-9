840182ac9591a46a6e5896bc8fc45d0d2cf9ac38
Dee Davis <threedaycleanse@gmail.com>
2017-03-23 09:14:09 -0400

    ControllerLab

--- unit_02/w07d04/student_labs/express-mongoose-lesson-starter/db/schema.js
+++ unit_02/w07d04/student_labs/express-mongoose-lesson-starter/db/schema.js
@@ -4,6 +4,22 @@ var Schema = mongoose.Schema;
 // Use native promises
 mongoose.Promise = global.Promise;
 
+var ProjectIdeaSchema = new Schema({
+  description: String,
+  in_progress: Boolean,
+  created_at: Date,
+  updated_at: Date
+});
+
+ProjectIdeaSchema.pre('save', function(next){
+  now = new Date();
+  this.updated_at = now;
+  if ( !this.created_at ) {
+    this.created_at = now;
+  }
+  next();
+});
+
 var ItemSchema = new Schema({
   name: String
 });
@@ -14,7 +30,8 @@ var UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   created_at: Date,
   updated_at: Date,
-  items: [ItemSchema]
+  items: [ItemSchema],
+  projectIdeas: [ProjectIdeaSchema]
 });
 
 UserSchema.pre('save', function(next){
@@ -29,8 +46,40 @@ UserSchema.pre('save', function(next){
 
 var UserModel = mongoose.model("User", UserSchema);
 var ItemModel = mongoose.model("Item", ItemSchema);
+var ProjectIdeaModel = mongoose.model("ProjectIdea", ProjectIdeaSchema);
 
 module.exports = {
   User: UserModel,
-  Item: ItemModel
+  Item: ItemModel,
+  ProjectIdea: ProjectIdeaModel
 };
+// var ItemSchema = new Schema({
+//   name: String
+// });
+//
+// var UserSchema = new Schema({
+//   first_name: String,
+//   last_name: String,
+//   email: { type: String, required: true, unique: true },
+//   created_at: Date,
+//   updated_at: Date,
+//   items: [ItemSchema]
+// });
+//
+// UserSchema.pre('save', function(next){
+//   now = new Date();
+//   this.updated_at = now;
+//   if ( !this.created_at ) {
+//     this.created_at = now;
+//   }
+//   next();
+// });
+//
+//
+// var UserModel = mongoose.model("User", UserSchema);
+// var ItemModel = mongoose.model("Item", ItemSchema);
+//
+// module.exports = {
+//   User: UserModel,
+//   Item: ItemModel
+// };
